1f38ff64cb95fa1842b4de0ecabcfa1b0f3f8376
Arty2001 <athavanth5@gmail.com>
2024-06-10 19:48:47 -0400

    dashboard Templates

--- components/DashboardLayout/index.tsx
+++ components/DashboardLayout/index.tsx
@@ -2,41 +2,82 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { IconHome, IconSettings, IconUser } from "@tabler/icons-react"; // Import icons from react-icons
+import { IconHome, IconSettings, IconUser, IconLogout } from "@tabler/icons-react";
+import Avatar from '@mui/material/Avatar';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
-  const navItems = [
+  const navItemsTop = [
     { icon: <IconHome />, path: "/dashboard" },
-    { icon: <IconSettings />, path: "/dashboard/settings" },
+  ];
+
+  const navItemsBottom = [
     { icon: <IconUser />, path: "/dashboard/profile" },
+    { icon: <IconSettings />, path: "/dashboard/settings" },
+    { icon: <IconLogout />, path: "/dashboard/logout" },
   ];
 
   return (
-    <div style={{ display: "flex", height: "100vh", boxSizing: 'border-box', padding: 20 }}>
+    <div style={{ display: "flex", height: "100vh", boxSizing: 'border-box', padding: "20px 10px" }}>
       <div style={{
-        boxShadow: '2px 0 5px rgba(0, 0, 0, 0.1)', // Add box shadow to navbar
+        boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px", // Add box shadow to navbar
         borderRadius: 20,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between", // Distribute space between the top and bottom sections
+        padding: "1rem 0",
       }}>
         <nav
           style={{
             width: "80px",
-            padding: "1rem 0",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
+            height: "100%",
           }}
         >
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 0 }}>
+            <div style={{ marginBottom: '60px' }}>
+              <Avatar alt="User Avatar" src="/path/to/avatar.jpg" />
+            </div>
+            <ul
+              style={{
+                listStyleType: "none",
+                padding: 0,
+                margin: 0,
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              {navItemsTop.map((item) => (
+                <li
+                  key={item.path}
+                  style={{
+                    marginBottom: "1rem",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link href={item.path}>{item.icon}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
           <ul
             style={{
               listStyleType: "none",
               padding: 0,
               margin: 0,
               width: "100%",
+              flexGrow: 1,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end", // Align the bottom icons to the bottom
             }}
           >
-            {navItems.map((item) => (
+            {navItemsBottom.map((item) => (
               <li
                 key={item.path}
                 style={{
